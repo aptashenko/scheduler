d6803b2d56524d08837e5b338893c6321a7920bc
Artem Ptashenko <aptashenko2019@gmail.com>
2026-05-17 02:31:59 +0200

    change structure add bot reminder

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const port = Number(process.env.PORT ?? 3000);
+
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`🚀 Server started on ${port}`);
 }
 bootstrap();
